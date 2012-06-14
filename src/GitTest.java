49024ef076b1ed429b4077c03bca3ff9e8c7c3a8
gwmccort <gwmccort@gmail.com>
2012-06-14 11:12:28 -0500

    test from work

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Git is fun and easy i think");
+		System.out.println("Git is fun and easy i think and from work");
 	}
 }
