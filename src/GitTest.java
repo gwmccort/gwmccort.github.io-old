88b70733ed4e71a19114179eb25592d1b3a14ee8
Glen <gwmccort@gmail.com>
2012-07-04 10:13:38 -0500

    This is a change on the eclipse branch

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// created w/ eclipse on branch
 		System.out.println("Git is fun and easy i think and from work and from home");
 	}
 }
