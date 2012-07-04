49b9cad5d1d16407ed155de5594c74fc26fcf3e3
Glen <gwmccort@gmail.com>
2012-07-04 17:45:36 -0500

    imported project to  netbeans

--- src/GitTest.java
+++ src/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// created w/ eclipse on branch
 		// this is on master
+            // updated from netbeans
 		System.out.println("Git is fun and easy i think and from work and from home");
 	}
 }
